afaa406b033308b362e305edc9355a374da4fa2d
Toshiyuki Oike <toshiyuki_oike@shino.ecei.tohoku.ac.jp>
2018-10-20 17:13:09 +0900

    add MainScreen

--- mobile/src/screens/MainScreen.tsx
+++ mobile/src/screens/MainScreen.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Container, Content, Button, Icon } from "native-base";
+import { Image } from "react-native";
+import { Container, Content, Button, Icon, Text } from "native-base";
 import { NavigationScreenProp } from "react-navigation";
 
-interface State {}
+interface State {
+  amount: number;
+}
 interface Props {
   navigation: NavigationScreenProp<any>;
 }
@@ -10,18 +13,55 @@ interface Props {
 export default class MainScreen extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
+
+    this.state = {
+      amount: 0
+    };
   }
 
   render() {
     return (
       <Container>
         <Content>
+          <Text
+            style={{
+              color: "#5cb85c",
+              alignSelf: "center",
+              fontSize: 60,
+              fontWeight: "bold"
+            }}
+          >
+            {"燃えるゴミ"}
+          </Text>
+          <Image
+            source={require("../assets/dustbox.png")}
+            style={{
+              width: 300,
+              height: 300,
+              marginTop: 50,
+              alignSelf: "center",
+              tintColor: "#5cb85c"
+            }}
+          />
+          <Text
+            style={{
+              color: "#5cb85c",
+              position: "absolute",
+              alignSelf: "center",
+              fontSize: 60,
+              fontWeight: "bold",
+              marginTop: 250
+            }}
+          >
+            {this.state.amount + "%"}
+          </Text>
           <Button
             transparent
             large
             onPress={() => {
               this.props.navigation.navigate("Setting");
             }}
+            success
           >
             <Icon name="settings" />
           </Button>
