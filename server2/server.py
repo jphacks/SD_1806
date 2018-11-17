0dbf50942bf74498e3b4b72e37b268d2cf406f70
fuurin <komatsuemgbr0624@gmail.com>
2018-11-17 13:21:53 +0900

    住所検索

--- server2/server.py
+++ server2/server.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+from entities import Entities
 from notification import FCMNotifier, NotificationTimer
 from collection_day_checker import todayIsCollectionDay, tomorrowIsCollectionDay
-from datetime import datetime as dt
-import numpy as np, datetime, json, os
-
+import os
 
 DEBUG = True
 
@@ -13,149 +10,27 @@ AMOUNT_THRESHOLD = 3
 SMELL_THRESHOLD = 500
 
 BASE_URL = os.environ.get('BASE_URL')
-FCM_API_KEY = os.environ.get('FCM_API_KEY') 
+RESET_DB_BY_INIT = os.environ.get('RESET_DB_BY_INIT')
+FCM_API_KEY = os.environ.get('FCM_API_KEY')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 DB_FILE = 'gomibako.db'
-
-app = Flask(__name__)
-app.debug = DEBUG
-
-db = SQLAlchemy(app)
-session = db.session
-
-db_url = os.environ.get('DATABASE_URL') or "sqlite:///" + os.path.join(app.root_path, DB_FILE)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-app.config['JSON_AS_ASCII'] = False
 
 notifier = FCMNotifier(FCM_API_KEY) if FCM_API_KEY else None
 ntimer = NotificationTimer(BASE_URL)
 
+app = Flask(__name__)
+app.debug = DEBUG
 
+db_url = DATABASE_URL or "sqlite:///" + os.path.join(app.root_path, DB_FILE)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['JSON_AS_ASCII'] = False
 
-class Amount(db.Model):
-    recorded = db.Column('recorded', db.DateTime(), primary_key=True, default=dt.now)
-    amount = db.Column('amount', db.Integer(), default='0', nullable=False)
-
-    def __init__(self, amount):
-        self.amount = amount
-
-    def add(self):
-        session.add(self)
-        session.commit()
-
-        return self.amount
-    
-    @classmethod
-    def latest(cls, limit=None):
-        rows = cls.query.order_by(desc(cls.recorded)).limit(limit or 1).all()
-        amounts = [{'recorded': row.recorded, 'amount': row.amount} for row in rows]
-
-        return amounts
-    
-    @classmethod
-    def total_this_month(cls):
-        now = dt.now()
-        this_month = datetime.date(now.year, now.month, 1)
-
-        rows = cls.query.filter(cls.recorded > this_month).all()
-        rows = np.array([amount.amount for amount in rows])
-
-        zero_idx = np.where(rows == 0)[0]
-        full_idx = zero_idx[zero_idx != 0] - 1
-
-        total = rows[full_idx].sum() + rows[-1]
-        
-        return int(total)
-
-
-
-class Smell(db.Model):
-    recorded = db.Column('recorded', db.DateTime(), primary_key=True, default=dt.now)
-    smell = db.Column('smell', db.Integer(), default='0', nullable=False)
-
-    def __init__(self, smell):
-        self.smell = smell
-
-    def add(self):
-        session.add(self)
-        session.commit()
-
-        return self.smell
-    
-    @classmethod
-    def latest(cls, limit=None):
-        rows = cls.query.order_by(desc(cls.recorded)).limit(limit or 1).all()
-        smells = [{'recorded': row.recorded, 'smell': row.smell} for row in rows]
-
-        return smells
-
-
-
-class Config(db.Model):
-    lineup = ['name', 'nth', 'weekday', 'notification', 'time']
-
-    name = db.Column(lineup[0], db.String(20), primary_key=True, default="普通ゴミ")
-    nth = db.Column(lineup[1], db.String(10))
-    weekday = db.Column(lineup[2], db.String(10))
-    notification = db.Column(lineup[3], db.String(1), default="1")
-    time = db.Column(lineup[4], db.String(5), default="07:00")
-
-    def __init__(self, configs):
-        self.configs = configs
-        
-        for key in configs.keys():
-            setattr(self, key, configs[key])
-        
-    def change(self):
-        config = self.query.first()
-
-        if config:
-            for key in self.configs: 
-                setattr(config, key, getattr(self, key))
-        else: 
-            session.add(self)
-        
-        session.commit()
-
-        return Config.get()
-
-    @classmethod
-    def get(cls):
-        row = cls.query.first()
-        if not row: return None
-
-        config = {}
-        for key in cls.lineup: 
-            config[key] = getattr(row, key)
-        
-        return config
-
-
-
-class Token(db.Model):
-    token = db.Column('token', db.String(), primary_key=True)
-
-    def __init__(self, token):
-        self.token = token
-    
-    def change(self):
-        token = self.query.first()
-
-        if token: 
-            token.token = self.token
-        else: 
-            session.add(self)
-
-        session.commit()
-
-        return self.token
-     
-    @classmethod
-    def get(cls):         
-        row = cls.query.first()
- 
-        return row.token if row else None
-
-
+ett = Entities(app)
+Amount = ett.Amount()
+Smell = ett.Smell()
+Config = ett.Config()
+Token = ett.Token()
+Collection = ett.Collection()
 
 
 
@@ -202,6 +77,21 @@ def smell():
 
 
 
+def update(form):
+    keys = form.keys()
+
+    if notifier:
+        if 'name' in keys: 
+            notifier.name = form['name']
+        
+        if 'notification' in keys:
+            notifier.notification = form['notification']
+
+    if 'time' in keys: 
+        ntimer.time = request.form['time']
+        ntimer.start()
+
+
 @app.route('/config', methods=['GET', 'POST'])
 def config(): 
     if request.method == 'GET':
@@ -211,18 +101,7 @@ def config():
         config = Config(request.form).change()
         response = jsonify({'registered config': config})
 
-        keys = request.form.keys()
-
-        if notifier:
-            if 'name' in keys: 
-                notifier.name = request.form['name']
-            
-            if 'notification' in keys:
-                notifier.notification = request.form['notification']
-
-        if 'time' in keys: 
-            ntimer.time = request.form['time']
-            ntimer.start()
+        update(request.form)
         
     return response
 
@@ -242,6 +121,36 @@ def token():
             notifier.token = token
 
     return response
+
+
+
+@app.route('/collection')
+def collection():
+    id = request.args.get('id')
+    sorting_id = request.args.get('sorting_id')
+
+    if not id or not sorting_id:
+        response = jsonify('idとsorting_idを指定してください．')
+        response.status_code = 400
+        return response
+
+    nth, weekday = Collection.nth_weekday(id, sorting_id)
+    result = {'nth': nth, 'weekday': weekday}
+
+    return jsonify(result)
+
+
+
+@app.route('/collection/search')
+def collection_search():
+    pref_id = request.args.get('pref_id') or 0
+    city_id = request.args.get('city_id') or 0
+    ku_id = request.args.get('ku_id')
+    kana1 = request.args.get('kana1')
+    kana2 = request.args.get('kana2')
+    result = Collection.search(pref_id, city_id, ku_id, kana1, kana2)
+
+    return jsonify(result)
 
 
 
@@ -278,21 +187,23 @@ def test_notify():
         if notifier.notify(msg):
             response = jsonify({
                 'msg': msg, 
-                'API_KEY': FCM_API_KEY, 
+                'api_key': FCM_API_KEY, 
                 'token': Token.get()
             })
     
     return response
 
+
+
 @app.before_first_request
 def initializing():
     print('initializing')
 
-    if os.environ.get('RESET_DB_BY_INIT') == '1':
+    if RESET_DB_BY_INIT == '1':
         print('reset DB')
-        db.drop_all()
-    
-    db.create_all()
+        ett.db.drop_all()
+        ett.db.create_all()
+        Collection.create()
 
     config = Config.get()
     
@@ -304,6 +215,8 @@ def initializing():
             
         ntimer.time = config['time']
         ntimer.start()
+
+
 
 if __name__ == '__main__':
     app.run()
