6e69148d687d16614497b290b5e7c28dd65508e0
小松秀生 <komatsu@mac.local>
2018-11-18 07:00:52 +0900

    commit

--- server/data_local.sh
+++ server/data_local.sh
@@ -2,12 +2,6 @@
 export FCM_API_KEY=AAAALX7eOX0:APA91bEo6y4e8UuH0fp_9SMkwTrKruwSKvTkle5wSW0vZn2i4Ws8HujeGQVU_IWZ8GV6jF1uAsxzQRhwIJ2-FF-JI-zKw2c4mq59bYo3BPZ1Y6UjsLOXlsQqpD0n4AukX8SV_Z4RnjlQ
 export RESET_DB_BY_DEPLOY=0
 
-curl -X POST -d "amount=0&datetime=2018-10-10 12:00:00" localhost:5000/amount
-curl -X POST -d "amount=1&datetime=2018-10-11 12:00:00" localhost:5000/amount
-curl -X POST -d "amount=2&datetime=2018-10-12 12:00:00" localhost:5000/amount
-curl -X POST -d "amount=3&datetime=2018-10-13 12:00:00" localhost:5000/amount
-curl -X POST -d "amount=4&datetime=2018-10-14 12:00:00" localhost:5000/amount
-curl -X POST -d "amount=0&datetime=2018-10-15 12:00:00" localhost:5000/amount
 
 curl -X POST -d "amount=3&datetime=2018-11-10 12:00:00" localhost:5000/amount
 curl -X POST -d "amount=0&datetime=2018-11-11 12:00:00" localhost:5000/amount
@@ -15,12 +9,58 @@ curl -X POST -d "amount=0&datetime=2018-11-12 12:00:00" localhost:5000/amount
 curl -X POST -d "amount=1&datetime=2018-11-13 12:00:00" localhost:5000/amount
 curl -X POST -d "amount=4&datetime=2018-11-14 12:00:00" localhost:5000/amount
 
+
+curl -X POST -d "amount=5&datetime=2018-10-10 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-10-11 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=5&datetime=2018-10-12 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-10-13 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=5&datetime=2018-10-14 12:00:00" localhost:5000/amount
+
+
+curl -X POST -d "amount=1&datetime=2018-09-10 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-09-11 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=1&datetime=2018-09-12 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-09-13 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=1&datetime=2018-09-14 12:00:00" localhost:5000/amount
+
+
+curl -X POST -d "amount=2&datetime=2018-08-10 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-08-11 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=2&datetime=2018-08-12 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-08-13 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=2&datetime=2018-08-14 12:00:00" localhost:5000/amount
+
+
+curl -X POST -d "amount=4&datetime=2018-07-10 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-07-11 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=4&datetime=2018-07-12 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-07-13 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=4&datetime=2018-07-14 12:00:00" localhost:5000/amount
+
+curl -X POST -d "amount=1&datetime=2018-06-10 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-06-11 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=1&datetime=2018-06-12 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-06-13 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=1&datetime=2018-06-14 12:00:00" localhost:5000/amount
+
+curl -X POST -d "amount=3&datetime=2018-06-10 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=3&datetime=2018-06-11 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=3&datetime=2018-06-12 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-06-13 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=1&datetime=2018-06-14 12:00:00" localhost:5000/amount
+
+curl -X POST -d "amount=5&datetime=2018-05-10 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-05-11 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=5&datetime=2018-05-12 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=0&datetime=2018-05-13 12:00:00" localhost:5000/amount
+curl -X POST -d "amount=5&datetime=2018-05-14 12:00:00" localhost:5000/amount
+
 curl -X POST -d "smell=100" localhost:5000/smell
 curl -X POST -d "smell=200" localhost:5000/smell
 curl -X POST -d "smell=300" localhost:5000/smell
 curl -X POST -d "smell=500" localhost:5000/smell
 curl -X POST -d "smell=150" localhost:5000/smell
-curl -X POST -d "smell=800" localhost:5000/smell
+curl -X POST -d "smell=100" localhost:5000/smell
 
 curl -X POST -d "name=プラごみ&nth=13&weekday=56&time=20:10" localhost:5000/config
 curl -X POST -d "token=abcdefg" localhost:5000/token
--- server/data_public.sh
+++ server/data_public.sh
@@ -1,22 +1,56 @@
 #!/bin/sh
-curl -X POST -d "amount=0" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=1" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=2" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=3" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=4" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=0" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=3" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=0" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=0" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=1" https://sugoigomibako.herokuapp.com/amount
-curl -X POST -d "amount=4" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=3&datetime=2018-11-10 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-11-11 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-11-12 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=1&datetime=2018-11-13 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=4&datetime=2018-11-14 12:00:00" https://sugoigomibako.herokuapp.com/amount
+
+
+curl -X POST -d "amount=5&datetime=2018-10-10 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-10-11 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=5&datetime=2018-10-12 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-10-13 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=5&datetime=2018-10-14 12:00:00" https://sugoigomibako.herokuapp.com/amount
+
+
+curl -X POST -d "amount=1&datetime=2018-09-10 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-09-11 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=1&datetime=2018-09-12 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-09-13 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=1&datetime=2018-09-14 12:00:00" https://sugoigomibako.herokuapp.com/amount
+
+
+curl -X POST -d "amount=2&datetime=2018-08-10 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-08-11 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=2&datetime=2018-08-12 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-08-13 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=2&datetime=2018-08-14 12:00:00" https://sugoigomibako.herokuapp.com/amount
+
+
+curl -X POST -d "amount=4&datetime=2018-07-10 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-07-11 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=4&datetime=2018-07-12 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-07-13 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=4&datetime=2018-07-14 12:00:00" https://sugoigomibako.herokuapp.com/amount
+
+curl -X POST -d "amount=1&datetime=2018-06-10 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-06-11 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=1&datetime=2018-06-12 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-06-13 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=1&datetime=2018-06-14 12:00:00" https://sugoigomibako.herokuapp.com/amount
+
+curl -X POST -d "amount=5&datetime=2018-05-10 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-05-11 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=5&datetime=2018-05-12 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=0&datetime=2018-05-13 12:00:00" https://sugoigomibako.herokuapp.com/amount
+curl -X POST -d "amount=5&datetime=2018-05-14 12:00:00" https://sugoigomibako.herokuapp.com/amount
 
 curl -X POST -d "smell=100" https://sugoigomibako.herokuapp.com/smell
 curl -X POST -d "smell=200" https://sugoigomibako.herokuapp.com/smell
 curl -X POST -d "smell=300" https://sugoigomibako.herokuapp.com/smell
 curl -X POST -d "smell=500" https://sugoigomibako.herokuapp.com/smell
 curl -X POST -d "smell=150" https://sugoigomibako.herokuapp.com/smell
-curl -X POST -d "smell=800" https://sugoigomibako.herokuapp.com/smell
+curl -X POST -d "smell=100" https://sugoigomibako.herokuapp.com/smell
 
 curl -X POST -d "name=plastic" https://sugoigomibako.herokuapp.com/config
 curl -X POST -d "token=abcdefg" https://sugoigomibako.herokuapp.com/token
